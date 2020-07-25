068b057b8fba96e185dabb46051d59b710518561
Hikotaro Abe <netseed@seedcreate.onmicrosoft.com>
2020-07-25 20:39:04 +0900

    Adjust unit test implementations.
    Span<uint> to XorShiftAddState.
    Existing tests are passed.
    
    Currently "XorShiftAddState" structure's  test is not implemented.

--- XorShiftAddSharpTest/XorShiftAddTest.cs
+++ XorShiftAddSharpTest/XorShiftAddTest.cs
@@ -14,13 +14,12 @@ namespace XorShiftAddSharpTest
         public XorShiftAddTest(ITestOutputHelper output) => _output = output;
 
 
-        static void AssertInternalVector(ReadOnlySpan<uint> expected, XorShiftAdd actual)
+        static unsafe void AssertInternalVector(in XorShiftAddState expected, XorShiftAdd actual)
         {
-            actual.State.Count.Is(4);
 
-            for (int i = 0; i < expected.Length; i++)
+            for (int i = 0; i < XorShiftAddState.Size; i++)
             {
-                expected[i].Is(actual.State[i]);
+                expected.Vector[i].Is(actual.State[i]);
             }
         }
 
@@ -69,15 +68,15 @@ namespace XorShiftAddSharpTest
         public void CtorTest()
         {
 
-            Span<uint> expected = stackalloc uint[4];
+            var expected = new XorShiftAddState();
 
-            XorShiftAddCore.Init(expected, 42);
+            XorShiftAddCore.Init(ref expected, 42);
             var actual = new XorShiftAdd(42);
             AssertInternalVector(expected, actual);
 
             var keys = new[] { 4u, 13u, 930u, 3767u, 31980u, 967285u, 3690813u, 85575140u, 106037230u, 3994571597u, };
 
-            XorShiftAddCore.Init(expected, keys);
+            XorShiftAddCore.Init(ref expected, keys);
             actual = new XorShiftAdd(keys);
             AssertInternalVector(expected, actual);
         }
@@ -97,16 +96,17 @@ namespace XorShiftAddSharpTest
         }
 
         [Fact]
-        public void TestNameB()
+        public unsafe void TestNameB()
         {
-            Span<uint> expected = stackalloc uint[4];
-            XorShiftAddCore.Init(expected, 42);
+            var expected=new XorShiftAddState();
 
-            var actual = XorShiftAdd.Restore(expected);
+            XorShiftAddCore.Init(ref expected, 42);
+
+            var actual = XorShiftAdd.Restore(new ReadOnlySpan<uint>(expected.Vector,XorShiftAddState.Size));
 
             for (var i = 0; i < actual.State.Count; ++i)
             {
-                actual.State[i].Is(expected[i]);
+                actual.State[i].Is(expected.Vector[i]);
             }
         }
 
@@ -115,12 +115,12 @@ namespace XorShiftAddSharpTest
         public void NextUint32Test()
         {
             var rnd = new XorShiftAdd(42);
-            Span<uint> state = stackalloc uint[XorShiftAddState.Size];
-            XorShiftAddCore.Init(state,42);
+            var state=new XorShiftAddState();
+            XorShiftAddCore.Init(ref state,42);
 
             for (int i = 0; i < 100; i++)
             {
-                var expected = XorShiftAddCore.NextUint32(state);
+                var expected = XorShiftAddCore.NextUint32(ref state);
 
                 rnd.NextUnsignedInt().Is(expected);
 
@@ -131,12 +131,12 @@ namespace XorShiftAddSharpTest
         public void NextFloatTest()
         {
             var rnd = new XorShiftAdd(42);
-            Span<uint> state = stackalloc uint[XorShiftAddState.Size];
-            XorShiftAddCore.Init(state, 42);
+            var state = new XorShiftAddState();
+            XorShiftAddCore.Init(ref state, 42);
 
             for (int i = 0; i < 100; i++)
             {
-                var expected = XorShiftAddCore.NextFloat(state);
+                var expected = XorShiftAddCore.NextFloat(ref state);
                 rnd.NextFloat().Is(expected);
             }
         }
@@ -144,15 +144,15 @@ namespace XorShiftAddSharpTest
         [Fact]
         public void NextTestA()
         {
-            Span<uint> state = stackalloc uint[XorShiftAddState.Size];
-            XorShiftAddCore.Init(state, 42);
+            var state = new XorShiftAddState();
+            XorShiftAddCore.Init(ref state, 42);
 
             XorShiftAdd rnd = new XorShiftAdd(42);
             const int maxValue = 100;
 
             for (int i = 0; i < 100; i++)
             {
-                var expected = (int) (XorShiftAddCore.NextDouble(state) * maxValue);
+                var expected = (int) (XorShiftAddCore.NextDouble(ref state) * maxValue);
                 rnd.Next(maxValue).Is(expected);
             }
 
@@ -162,8 +162,8 @@ namespace XorShiftAddSharpTest
         [Fact]
         public void NextTestB()
         {
-            Span<uint> state = stackalloc uint[XorShiftAddState.Size];
-            XorShiftAddCore.Init(state, 42);
+            var state = new XorShiftAddState();
+            XorShiftAddCore.Init(ref state, 42);
 
             XorShiftAdd rnd = new XorShiftAdd(42);
 
@@ -172,7 +172,7 @@ namespace XorShiftAddSharpTest
 
             for (int i = 0; i < 100; i++)
             {
-                var expected = (int) ((long) (XorShiftAddCore.NextDouble(state) * (maxValue - minValue)) + minValue);
+                var expected = (int) ((long) (XorShiftAddCore.NextDouble(ref state) * (maxValue - minValue)) + minValue);
                 var act = rnd.Next(minValue, maxValue);
                 act.Is(expected);
             }
@@ -181,42 +181,42 @@ namespace XorShiftAddSharpTest
         }
 
         [Fact]
-        public void JumpTestA()
+        public unsafe void JumpTestA()
         {
             const string bs = "ffff";
             const int ms = 100;
 
 
             var rnd = new XorShiftAdd(42);
-            Span<uint> expected = stackalloc uint[4];
-            XorShiftAddCore.Init(expected, 42);
+            var expected = new XorShiftAddState();
+            XorShiftAddCore.Init(ref expected, 42);
 
             var actual = rnd.Jump(ms, bs);
-            XorShiftAddCore.Jump(expected, ms, bs);
+            XorShiftAddCore.Jump(ref expected, ms, bs);
 
-            for (int i = 0; i < expected.Length; i++)
+            for (int i = 0; i <XorShiftAddState.Size; i++)
             {
-                actual.State[i].Is(expected[i]);
+                actual.State[i].Is(expected.Vector[i]);
             }
 
         }
 
         [Fact]
-        public void JumpTestB()
+        public unsafe void JumpTestB()
         {
             const string jumbStr = "5b0abc7da8055ce3aef263ccb271d12e";
 
             var rnd = new XorShiftAdd(42);
-            
-            Span<uint> expected = stackalloc uint[4];
-            XorShiftAddCore.Init(expected, 42);
-            XorShiftAddCore.Jump(expected, jumbStr);
+
+            var expected = new XorShiftAddState();
+            XorShiftAddCore.Init(ref expected, 42);
+            XorShiftAddCore.Jump(ref expected, jumbStr);
 
             var actual = rnd.Jump(jumbStr);
 
-            for (int i = 0; i < expected.Length; i++)
+            for (int i = 0; i < XorShiftAddState.Size; i++)
             {
-                actual.State[i].Is(expected[i]);
+                actual.State[i].Is(expected.Vector[i]);
             }
 
         }
@@ -224,22 +224,22 @@ namespace XorShiftAddSharpTest
         [Fact]
         public void NextDoubleTest()
         {
-            Span<uint> expected = stackalloc uint[4];
-            XorShiftAddCore.Init(expected, 114514);
+            var expected = new XorShiftAddState();
+            XorShiftAddCore.Init(ref expected, 114514);
 
             var rnd = new XorShiftAdd(114514);
 
             for (int i = 0; i < 100; i++)
             {
-                rnd.NextDouble().Is(XorShiftAddCore.NextDouble(expected));
+                rnd.NextDouble().Is(XorShiftAddCore.NextDouble(ref expected));
             }
         }
 
         [Fact]
         public void NextBytesTestA()
         {
-            Span<uint> expected = stackalloc uint[4];
-            XorShiftAddCore.Init(expected, 114514);
+            var expected = new XorShiftAddState();
+            XorShiftAddCore.Init(ref expected, 114514);
 
             var rnd = new XorShiftAdd(114514);
             var actual = new byte[128];
@@ -248,15 +248,15 @@ namespace XorShiftAddSharpTest
 
             foreach (var i in actual)
             {
-                i.Is((byte) XorShiftAddCore.NextUint32(expected));
+                i.Is((byte) XorShiftAddCore.NextUint32(ref expected));
             }
         }
 
         [Fact]
         public void NextBytesTestB()
         {
-            Span<uint> expected = stackalloc uint[4];
-            XorShiftAddCore.Init(expected, 114514);
+            var expected = new XorShiftAddState();
+            XorShiftAddCore.Init(ref expected, 114514);
 
             var rnd = new XorShiftAdd(114514);
             Span<byte> actual = stackalloc byte[128];
@@ -265,7 +265,7 @@ namespace XorShiftAddSharpTest
 
             foreach (var i in actual)
             {
-                i.Is((byte)XorShiftAddCore.NextUint32(expected));
+                i.Is((byte)XorShiftAddCore.NextUint32(ref expected));
             }
 
         }
