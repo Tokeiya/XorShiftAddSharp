1240b6857b720b15d1d4b3f046ac04e719a6e29c
Hikotaro Abe <netseed@seedcreate.onmicrosoft.com>
2020-07-26 20:33:46 +0900

    add package.

---
+++ XorShiftAddSharp/XorShiftAddPooledObjectPolicy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.ObjectPool;
+
+namespace XorShiftAddSharp
+{
+    public sealed class XorShiftAddPooledObjectPolicy : PooledObjectPolicy<XorShiftAdd>
+    {
+        public override XorShiftAdd Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Return(XorShiftAdd obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
